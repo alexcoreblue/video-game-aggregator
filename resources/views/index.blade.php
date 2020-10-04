6664983a29cd0cf3f9fc31de944ca6038d5e06d4
Alex Maclean <alex@coreblue.co.uk>
2020-10-04 17:40:14 +0100

    Allow for popular games to be rendered dynamically from the IGDB API

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,109 +7,26 @@
 
     <div class="popular-games text-sm grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
 
+        @foreach ($highestRatedGames as $game)
         <div class="game mt-8">
             <div class="relative inline-block">
                 <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
                 <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
                     <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                 </div>
             </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name'] }}</a>
+            <div class="text-gray-400 mt-1">
+                @foreach ($game['platforms'] as $platform)
+                @if (array_key_exists('abbreviation', $platform))
+                {{ $platform['abbreviation'] }},
+                @endif
+                @endforeach
             </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
         </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
-
-        <div class="game mt-8">
-            <div class="relative inline-block">
-                <a href="#">
-                    <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                </div>
-            </div>
-            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
-            <div class="text-gray-400 mt-1">Playstation 4</div>
-        </div>
+        @endforeach
 
     </div> <!-- end popular-games -->
 
