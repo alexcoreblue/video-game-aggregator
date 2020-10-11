62325851673928228e1c572938508ae8a633759f
Alex Maclean <alex@coreblue.co.uk>
2020-10-11 16:17:12 +0100

    Implement blade components to remove any repeated code from the views

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -162,28 +162,7 @@
         <div class="similar-games text-sm grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-12">
 
             @foreach ($game['similarGames'] as $game)
-            <div class="game mt-8">
-                <div class="relative inline-block">
-
-                    <a href="#">
-                        <img src="{{ $game['coverImageUrl'] }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            @if ($game['rating'])
-                            {{ $game['rating'] }}
-                            @else
-                            N/A
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name'] }}</a>
-                <div class="text-gray-400 mt-1">
-                    {{$game['platforms'] }}
-                </div>
-            </div>
+            <x-game-card :game="$game" />
             @endforeach
 
         </div> <!-- end similar games -->
